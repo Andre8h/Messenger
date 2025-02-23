5524cd2da375247b2cc8dd95c4557a6850621ca9
Andre <andresquinones71@gmail.com>
2025-02-23 18:10:55 -0500

    Finalization of the updatProfile endpoint

--- Back-end/src/controllers/auth.controller.js
+++ Back-end/src/controllers/auth.controller.js
@@ -84,3 +84,21 @@ export const logout = (req, res) => {
         res.status(500).json({message: "Internal Server Error"});
     }
 }
+
+export const updateProfile = async (req,res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+        if(!profilePic){
+            return res.status(400).json({message: "profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updateUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url},{new:true})
+
+        res.status(200).json(updateUser)
+    } catch (error) {
+        console.log("Error in Update controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
